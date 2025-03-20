f255728900e9a31ac972b822bb777a2e049863e8
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-03-20 11:15:09 +0300

    users requests

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 
+//MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
+
+// ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -87,6 +92,67 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getUser = (req, res) => {
+  const tourId = req.params.tourId * 1;
+  const tour = tours.find((el) => el.id === tourId);
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const updateUser = (req, res) => {
+  const tourId = req.params.tourId * 1;
+  const tour = tours.find((el) => el.id === tourId);
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const deleteUser = (req, res) => {
+  const tourId = req.params.tourId * 1;
+  const tour = tours.filter((el) => el.id === tourId);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:tourId')
@@ -94,5 +160,13 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+app.route('/api/v1/users').get(getAllUsers);
+app
+  .route('/api/v1/users/:userId')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// SERVER
 const port = 3000;
 app.listen(port, () => console.log(`App running on port ${port}`));
