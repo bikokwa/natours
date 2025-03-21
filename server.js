27e82af366020be8eab9b0858658389655fe8d50
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-03-21 10:41:44 +0300

    connect database with express app

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+// DATABASE
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 // SERVER
 const port = 3000;
