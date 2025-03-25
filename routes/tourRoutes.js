98bfe616bf7608b9b254edf81d54b376debdff89
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-03-25 07:30:41 +0300

    group aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTop5CheapTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
