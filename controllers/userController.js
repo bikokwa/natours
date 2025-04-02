1859ecf1658b514033d3618d3c6328dd2fdd7fb1
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-02 21:34:18 +0300

    delete current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,8 +61,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     );
   }
 
-  // update user document
+  // Filtered out field names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
@@ -75,6 +77,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     data: {
       user: updatedUser
     }
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
