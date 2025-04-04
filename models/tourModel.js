dd71b28913fe1849c3d2e2d1d1306f53195e5eb1
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-04 20:57:44 +0300

    child reference tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const User = require('./userModel');
-
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -99,7 +97,12 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -113,12 +116,6 @@ tourSchema.virtual('durationWeeks').get(function() {
 
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
