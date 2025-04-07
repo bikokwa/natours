171ffe0a8909e52ead67469fd336e5dbee087467
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-07 08:21:00 +0300

    create new review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -6,8 +6,20 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
+    results: reviews.length,
     data: {
       reviews
+    }
+  });
+});
+
+exports.createNewReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview
     }
   });
 });
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const ReviewController = require('./../controllers/reviewController');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(ReviewController.getAllReviews);
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createNewReview
+  );
 
 module.exports = router;
