ce6d78c27ac2f54cf56cbb89f650d08dffb08f35
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-08 08:31:44 +0300

    create factory function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,17 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const newDoc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+  });
+
 exports.updateOne = Model =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
