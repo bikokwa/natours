34065d0f4c1c5b7a9b44018e32fa727ac6b1aa16
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-08 06:17:49 +0300

    nested routes with express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -23,14 +25,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createNewReview
   );
 
 module.exports = router;
