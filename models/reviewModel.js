94d93a7d558cb13493f885abd0db7f3b4d95c676
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-09 05:47:31 +0300

    update and delete review stats on tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -55,15 +55,34 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
       }
     }
   ]);
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating
-  });
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
 };
 
 reviewSchema.post('save', function() {
   // this points to current review document
   this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.currentReview = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  await this.currentReview.constructor.calcAverageRatings(
+    this.currentReview.tour
+  );
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
