77f65fa1d8fc727c79ce11a13ec7a8187cc1788c
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-04-25 17:55:49 +0300

    the Tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,7 +15,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
+  // Get the data for the requested tour including reviews and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  // Build template
+
+  // Render template
   res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+    title: tour.name,
+    tour
   });
 });
