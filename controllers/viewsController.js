114f7410b915ff1832b22dc5fb92669853c1507b
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-05-02 16:48:24 +0300

    render error pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // Get tour data from collection
@@ -21,7 +22,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user'
   });
 
-  // Build template
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
 
   // Render template
   res.status(200).render('tour', {
