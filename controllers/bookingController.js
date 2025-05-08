35b2a39f8948fab9e5a65d191c1d48637a07fab4
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-05-08 13:09:05 +0300

    integrate stripe into the backend

---
+++ controllers/bookingController.js
@@ -0,0 +1,39 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourID);
+
+  // Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: `${tour.name}`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`]
+          },
+          unit_amount: tour.price * 100
+        },
+        quantity: 1
+      }
+    ]
+  });
+  // Create session as response
+  res.status(200).json({
+    status: 'success',
+    session
+  });
+});
