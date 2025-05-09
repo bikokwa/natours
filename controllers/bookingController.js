eaf5fb6a80955e20ffc0bb69f8101bdc1d0229a2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-05-09 13:48:29 +0300

    finish bookings API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,6 +4,7 @@ const Booking = require('./../models/bookingModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // Get the currently booked tour
@@ -50,3 +51,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
